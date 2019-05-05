6fea87b394ddb209d9ec04b444c7d556dbcc4991
TAGEER <47475033+TAGEER@users.noreply.github.com>
2019-05-06 02:24:13 +0300

    Update RUNTA.sh

--- RUNTA.sh
+++ RUNTA.sh
@@ -7,31 +7,31 @@ install() {
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 
-  sudo apt-get install g++-4.7 -y c++-4.7 -y
+Â Â sudo apt-get install g++-4.7 -y c++-4.7 -y
 
 
-  sudo apt-get update
+Â Â sudo apt-get update
 
 
-  sudo apt-get upgrade
+Â Â sudo apt-get upgrade
 
 
-  sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+Â Â sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
 
 
-  sudo apt-get install screen -y
+Â Â sudo apt-get install screen -y
 
 
-  sudo apt-get install tmux -y
+Â Â sudo apt-get install tmux -y
 
 
-  sudo apt-get install libstdc++6 -y
+Â Â sudo apt-get install libstdc++6 -y
 
 
-  sudo apt-get install lua-lgi -y
+Â Â sudo apt-get install lua-lgi -y
 
 
-  sudo apt-get install libnotify-dev -y
+Â Â sudo apt-get install libnotify-dev -y
 
 
 wget "https://valtman.name/files/telegram-cli-1222"
@@ -40,11 +40,11 @@ mv telegram-cli-1222 tg
 
 sudo chmod +x tg
 
-echo -e " BY TEAM FAEDER"
+echo -e " BY TEAM TAGEER"
 
-echo -e " BY TEAM FAEDER" 
+echo -e " BY TEAM TAGEER" 
 
-echo -e " BY TEAM FAEDER" 
+echo -e " BY TEAM TAGEER" 
 
 }
 
